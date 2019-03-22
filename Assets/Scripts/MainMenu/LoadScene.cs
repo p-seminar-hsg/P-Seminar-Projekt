664d8ba5a0424092f9015db4b66114b5f211887a
Luca <lukellmann@gmail.com>
2019-03-23 00:58:09 +0100

    Created MainMenu
    MainMenu erstellt.
    TODO:
    - Beispielgrafiken ersetzen
    - richtigen sceneIndex für StartButton wählen
    - Inhalt von ImpressumPanel und ControlsPanel
    - Funktion von Slider und Highscore

---
+++ Assets/Scripts/MainMenu/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿
+/*Ersteller: Luca Kellermann
+    Zuletzt geändert am: 22.03.2019
+    Funktion: Dieses Script dient zum Öffnen einer anderen Scene durch das MainMenu*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour{
+    
+    //Läd die Scene mit dem übergebenen Index
+    public void LoadSceneByIndex(int sceneIndex){
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
