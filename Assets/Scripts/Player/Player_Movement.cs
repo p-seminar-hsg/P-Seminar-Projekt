3d3aaf02dcfa1e121e629392cd6838d2a4e56147
Florian Müller-Martin <florian.mm02@gmail.com>
2019-05-23 17:32:19 +0200

    Upload Player
    This commit contains the Player Prefab, it's Scripts, a Boundary Prefab, a Player UI Prefab containing a Joystick as well as a attack-button and the joystick Plugin which is used in the UI.

---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Ersteller: Florian Müller-Martin und Tobias Schwarz
+ * Zuletzt geändert am 23.05.2019
+ * Funktion: Dieses Skript ist für die Bewegung des Players mithilfe des Joysticks zuständig
+ */
+
+public class Player_Movement : MonoBehaviour
+{
+    public Joystick joystick; //Referenz zum GameObject MovementJoystick
+    public int speed; //Geschwindigkeit des Players
+    public Rigidbody2D rigidbody; //Rigidbody des Players
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // FixedUpdate wird einmal pro Frame aufgerufen und fragt jedes mal die Position des Joysticks ab. Diese Position wird dann in eine Bewegung für den Player umgerechnet.
+    void FixedUpdate()
+    {
+        //Vector2 move = new Vector2(joystick.Horizontal * speed, joystick.Vertical * speed);
+        //transform.Translate(move);
+
+        rigidbody.velocity = new Vector2(10000 * Time.deltaTime * joystick.Horizontal * speed, 10000 * Time.deltaTime * joystick.Vertical * speed);
+        
+    }
+
+
+    //OnCollsionEnter wird ausgelöst, wenn der Player eine collision detektiert. Wenn der collider als Boundary getagged ist, dann wird die Bewegung des Players eingeschränkt.
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "boundaryX") {
+
+            rigidbody.velocity = new Vector2(0, 10000 * Time.deltaTime * joystick.Vertical * speed);
+       }
+
+        if (collision.collider.tag == "boundaryY")
+        {
+
+            rigidbody.velocity = new Vector2(10000 * Time.deltaTime * joystick.Horizontal * speed, 0);
+        }
+    }
+
+    //Die Geschwindigkeit des Players erhöht sich um den übergebenen Wert
+   public void speedUp (int speedUp)
+    {
+        speed += speedUp;
+    }
+
+}
