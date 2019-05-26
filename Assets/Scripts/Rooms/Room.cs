65407196c512a68bbe633662e6e6514e71638a7e
Luca <lukellmann@gmail.com>
2019-05-26 07:16:10 +0200

    Configured Rooms Test-Scene & more
    -configured Rooms Test-Scene
    -integrated RoomData into MapManager
    -added functionality: previous 3 Rooms won't be loaded
    -added functionality: Teleporter just teleport GameObjects with Player-Tag
    -created RoomFader

--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+/*Ersteller: Benedikt Wille und Luca Kellermann 
+    Zuletzt geändert am: 26.05.2019
+    Funktion: Dieses Script ist die Grundlage aller Räume.*/
+
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    //die Positionen des PlayerSpawns und Teleporters speichern
     public Transform playerSpawn;
     public Teleporter teleporter;
 
@@ -17,5 +21,11 @@ public class Room : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //Teleporter aktivieren/deaktivieren (z.B. nachdem alle Gegner besiegt wurden)
+    public void SetTeleporterActive(bool b)
+    {
+        teleporter.gameObject.SetActive(b);
     }
 }
