51e134c0a612461e629dfe837f41794f10cd55fb
Florian Müller-Martin <florian.mm02@gmail.com>
2019-06-02 22:25:53 +0200

    Upload Player Animations
    Walking animations for the player.

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -12,6 +12,7 @@ public class Player_Movement : MonoBehaviour
     public Joystick joystick; // Referenz zum GameObject MovementJoystick
     public int speed; // Geschwindigkeit des Players
     public Rigidbody2D rb; // Rigidbody des Players
+    public Animator animator; // Animator des Players
     
 
     // Start is called before the first frame update
@@ -23,22 +24,25 @@ public class Player_Movement : MonoBehaviour
     // FixedUpdate wird einmal pro Frame aufgerufen und fragt jedes mal die Position des Joysticks ab. Diese Position wird dann in eine Bewegung für den Player umgerechnet.
     void FixedUpdate()
     {
-        //Vector2 move = new Vector2(joystick.Horizontal * speed, joystick.Vertical * speed);
-        //transform.Translate(move);
-
+        //Die Geschwindigkeit des Rigidbodys wird je nach position des Joysticks eingestellt
         rb.velocity = new Vector2(Time.deltaTime * joystick.Horizontal * speed, Time.deltaTime * joystick.Vertical * speed);
-        
+
+        //
+        animator.SetFloat("speed_horizontal", joystick.Horizontal);
+        animator.SetFloat("speed_vertical", joystick.Vertical);
     }
 
 
     //OnCollsionEnter wird ausgelöst, wenn der Player eine collision detektiert. Wenn der collider als Boundary getagged ist, dann wird die Bewegung des Players eingeschränkt.
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //Bei einer Collision mit einem als "boundaryX" getaggten GameObject wird die x-Bewegung eingeschränkt
         if (collision.collider.tag == "boundaryX") {
 
             rb.velocity = new Vector2(0, Time.deltaTime * joystick.Vertical * speed);
-       }
+        }
 
+        //Bei einer Collision mit einem als "boundaryX" getaggten GameObject wird die x-Bewegung eingeschränkt
         if (collision.collider.tag == "boundaryY")
         {
 
