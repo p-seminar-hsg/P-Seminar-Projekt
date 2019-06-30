52afddef3cfb01f7c230b1b7c4807e893bfdf221
BelmontR <47500226+BelmontR@users.noreply.github.com>
2019-06-30 17:53:00 +0200

    PauseMenu hinzugefügt
    Das PauseMenu wurde hinzugefügt und auch in die UI Hierachie sinnvoll eingeführt. Das Menu wurde animiert und besitzt einen Knopf, um beschworen zu werden

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ Ersteller: Rene Jokiel
+ Zuletzt geändert am: 30.6.2019
+ Funktion: Diese Datei aktiviert das Inventar/ Pausemenü und deaktiviert die PlayerUI.
+           Ist auch für andere UIs verwendbar                   
+*/
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject ui;   //Zu aktivierende ui
+    public GameObject ui2;    // Zu deaktivierende ui. Nur notwendig, wenn die UIs in
+                              // separaten Canvas(es?) sind
+    public bool gameOver = false;
+
+    public void Toggle()    // Aktiviert ui1 und deaktiviert ui2
+    {
+        if (gameOver == false)
+        {
+            ui.SetActive(!ui.activeSelf);
+            ui2.SetActive(!ui2.activeSelf);
+
+            if (ui.activeSelf)
+            {
+                Time.timeScale = 0f;    //Zeit wird gestoppt
+            }
+            else
+            {
+                Time.timeScale = 1;     //Zeit wird normalisiert
+            }
+        }
+    }
+}
+    
