5ca60a4312ecb50da6467cad6db2bdaad7278124
Lukellmann <47486203+Lukellmann@users.noreply.github.com>
2019-07-13 01:14:24 +0200

    GameOver Score & Background +  Bug Prevent (#26)
    * GameOver Score & Background
    
    - GameOver Scene will now display the current Score
    - Added a Background to RoomScene
    - Fixed small Bug in EnemyAI (now nothing will happen if Player-Reference isn't assigned)
    
    * Bug Prevent
    
    - Prevented Bug, that Enemies can go through Walls (reverted change of CollisionTilemap Layer which would have caused the EnemyAI Script to not work properly)

--- Assets/Scripts/RoomSceneUI/GameOver.cs
+++ Assets/Scripts/RoomSceneUI/GameOver.cs
@@ -1,12 +1,11 @@
+
 ﻿/// <summary>
 /// Ersteller: Rene Jokiel;
 /// Zuletzt geändert am: 12.07.2019
 /// 
-/// Script, das für das Erscheinen des GameOver Screens zuständig ist
+/// Script, das für das Erscheinen des GameOver Screens zuständig ist.
 /// </summary>
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -16,12 +15,13 @@ public class GameOver : MonoBehaviour
     public GameObject PlayerUI;
     public GameObject PauseMenuButton;
     public GameObject GameOverUI;
+    public Text scoreNumber;
 
 
     public RoomFader sceneFader;
 
     /// <summary>
-    /// Zuständig für den Retry Button. Lädt die aktuelle Spielszene neu
+    /// Zuständig für den Retry Button. Lädt die aktuelle Spielszene neu.
     ///<summary>
     public void Retry()
     {
@@ -32,17 +32,19 @@ public class GameOver : MonoBehaviour
         GameOverUI.SetActive(GameOverUI.activeSelf);
     }
     /// <summary>
-    /// Toggelt den GameOver Screen
+    /// Toggelt den GameOver Screen.
     ///<summary>
     public void GoGameOver()
     {
         PlayerUI.SetActive(!PlayerUI.activeSelf);
         PauseMenuButton.SetActive(false);
+        //den Score im GameOver Screen aktualisieren
+        scoreNumber.text = GameManager.GetScore().ToString();
         GameOverUI.SetActive(true);
     }
 
     /// <summary>
-    /// Lädt das Hauptmenü
+    /// Lädt das Hauptmenü.
     ///<summary>
     public void MainMenu()
     {
