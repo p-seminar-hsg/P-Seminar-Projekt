9b22e4a633547a6cbf623efb400a4c3629415748
BelmontR <47500226+BelmontR@users.noreply.github.com>
2019-07-29 00:17:45 +0200

    Added a new way to spawn items by using a destroyable box
    I don't really know why I just did this, but I did it, so, I won't complain about it

---
+++ Assets/Scenes/Box.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Ersteller: Rene Jokiel
+/// Zuletzt geändert am 29.07.2019
+/// Script für eine Box, die bei Zerstörung ein Item fallen lässt
+/// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public Item itemToSpawn;
+
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        if (Other.CompareTag("AttackHitbox"))
+        {
+            Item object_spawn = (Item)Instantiate(itemToSpawn, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
