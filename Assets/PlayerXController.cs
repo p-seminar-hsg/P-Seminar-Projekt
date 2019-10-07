d72faee0cbe9b09ccc5493b489e495874ad0da20
BelmontR <47500226+BelmontR@users.noreply.github.com>
2019-10-07 20:56:04 +0200

    Well... Let's call it... controller adaption... Or the beginning of this

---
+++ Assets/PlayerXController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerXController : MonoBehaviour
+{
+    NewControls controls;
+    public Player_Main playerMain;
+
+    Vector2 move;
+
+    private void Awake()
+    {
+        controls = new NewControls();
+
+        controls.Gameplay.Attack.performed += ctx => playerMain.attackMain();
+        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => move = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    private void Update()
+    {
+        Vector2 m = new Vector2(move.x, move.y) * Time.deltaTime;
+        transform.Translate(m, Space.World);
+    }
+
+}
---
+++ Assets/Scripts/New Controls.cs
@@ -0,0 +1,160 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Design/KnittlRadSpritesheets/New Controls.inputactions'
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class NewControls : IInputActionCollection
+{
+    private InputActionAsset asset;
+    public NewControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""New Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""f944d118-f6a9-46b9-8b8f-641b73cd2c50"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Button"",
+                    ""id"": ""24060aed-a5ce-4ba9-91f8-0f54c5797710"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""799cf5d0-a083-48a7-ab9a-e2f392679155"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""eec78c6c-6339-4518-9bb4-37411b39afa3"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7b5b3c6f-d049-481f-a3b2-a54b516eadb1"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        m_Gameplay_Attack = m_Gameplay.FindAction("Attack", throwIfNotFound: true);
+    }
+
+    ~NewControls()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Move;
+    private readonly InputAction m_Gameplay_Attack;
+    public struct GameplayActions
+    {
+        private NewControls m_Wrapper;
+        public GameplayActions(NewControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputAction @Attack => m_Wrapper.m_Gameplay_Attack;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Attack.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+                Attack.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+                Attack.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Move.started += instance.OnMove;
+                Move.performed += instance.OnMove;
+                Move.canceled += instance.OnMove;
+                Attack.started += instance.OnAttack;
+                Attack.performed += instance.OnAttack;
+                Attack.canceled += instance.OnAttack;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnAttack(InputAction.CallbackContext context);
+    }
+}
