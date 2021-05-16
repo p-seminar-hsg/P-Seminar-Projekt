6fdc779361dd5f208c78b120eb8013e55d264751
BelmontR <47500226+BelmontR@users.noreply.github.com>
2021-05-16 03:17:39 +0200

    Added Room without Walls
    @FlorianGanserFG  @FlorianMM  @Glaswartron  @Lukellmann  @PSeminar18-20  @TobidobiS  @joshuabrandes  @matze090301  @patrick0301
    
    Co-Authored-By: Lukellmann <47486203+Lukellmann@users.noreply.github.com>

---
+++ Assets/Hi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hi : MonoBehaviour
+{
+    public GameObject hidiho;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hidiho.SetActive(true);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        hidiho.SetActive(false);
+    }
+}
